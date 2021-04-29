8ac5abb9f1ad4ecd528d05259c37b5d76e96b9d7
navid-ahmed <nahme6@gmail.com>
2021-04-30 04:01:55 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tools_Library_Application_Software
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
