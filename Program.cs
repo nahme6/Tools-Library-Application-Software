ec3f882ac9d34df19ffcb195ce36fb1c0bc4eda5
navidAhmed <nahme6@gmail.com>
2021-05-24 23:11:04 +1000

    Project Complete

--- Program.cs
+++ Program.cs
@@ -7,151 +7,27 @@ namespace Tools_Library_Application_Software
         public static ToolLibrarySystem toolLibrarySystem;
         static void Main(string[] args)
         {
-
-
             Tool tool1 = new Tool("Irwin 125mm orbital sander", 5, 3, 2);
             Tool tool2 = new Tool("Rocket sanding block holder", 2, 2, 1);
             Tool tool3 = new Tool("Powerfit 120 triangular sander", 1, 1, 5);
-            Tool tool4 = new Tool("Chain sawwww", 5, 2, 0);
 
             Member member1 = new Member("Navid", "Ahmed", "0450077117", "0000");
+            Member member2 = new Member("Tom", "Riddle", "046523075", "4444");
+            Member member3 = new Member("Tony", "Stark", "045028858", "0567");
 
             toolLibrarySystem = new ToolLibrarySystem();
 
             toolLibrarySystem.add(tool1, ToolTypes.basicHandTools);
             toolLibrarySystem.add(tool2, ToolTypes.basicHandTools);
             toolLibrarySystem.add(tool3, ToolTypes.basicHandTools);
-            toolLibrarySystem.add(tool4, ToolTypes.gardenPowerTools);
 
-            toolLibrarySystem.add(member1);
-            toolLibrarySystem.borrowTool(member1, tool1);
-
-            new MainMenu();
-
-            /*
-            string[] mainMenuOptions = { "Staff Login", "Member Login", "Exit" };
-            Menu menu = new Menu("Main Menu", mainMenuOptions);
-            menu.printMenu();
-
-            mainMenuInput = Console.ReadLine();
-            Console.WriteLine();
-
-            while (mainMenuInput != "0")
-            {
-                switch (mainMenuInput)
-                {
-                    case "1":
-                        string[] staffMenuOptions = {
-                            "Add a new tool",
-                            "Add new pieces of an existing tool",
-                            "Remove some pieces of a tool",
-                            "Register a new member",
-                            "Remove a member",
-                            "Find the contact number of a member",
-                            "Return to Main Menu"};
-                        Menu staffMenu = new Menu("Staff Menu", staffMenuOptions);
-                        staffMenu.printMenu();
-
-                        staffMenuInput = Console.ReadLine();
-                        Console.WriteLine();
-                            switch (staffMenuInput)
-                            {
-                                case "1":
-                                    new ToolCategoryMenu();
-
-                                    string input = Console.ReadLine();
-                                    Console.WriteLine();
-                                    
-                                    switch "1":
-                                    new 
-
-
-                                     break;
-                            }
-                    break;
-                }
-            }
-
-
-            Environment.Exit(0);
-
-            /*
-            MemberCollection memberCollection = new MemberCollection();
-            Member member1 = new Member("Navid", "Ahmed", "0450077117", "0000");
-            memberCollection.add(member1);
-            Member member2 = new Member("Chelsea", "Jenkins", "044565437", "7854");
-            memberCollection.add(member2);
-
-            Console.WriteLine(member1.ToString());
-            Console.WriteLine(member2.ToString());
-
-            ToolCollection toolCollection = new ToolCollection(20);
-            Tool tool1 = new Tool("Chainsaw", 5, 5, 0);
-            toolCollection.add(tool1);
-            Tool tool2 = new Tool("Hammer", 7, 6, 0);
-            toolCollection.add(tool2);
-
-            Console.WriteLine(tool1.ToString());
-            Console.WriteLine(tool2.ToString());
-
-            member1.addTool(tool1);
-            member2.addTool(tool2);
-            */
-
-            /*
-            ToolLibrarySystem toolLibrarySystem = new ToolLibrarySystem();
-
-            
-
-            Member member1 = new Member("Navid", "Ahmed", "0450077117", "7777");
-            Member member2 = new Member("Steve", "Rogers", "0450248454", "0000");
-            Member member3 = new Member("Hannah", "Montana", "045365333", "4778");
-
-
-            toolLibrarySystem.add(tool1, ToolTypes.basicHandTools);
-            toolLibrarySystem.add(tool2, ToolTypes.basicHandTools);
-            toolLibrarySystem.add(tool3, ToolTypes.basicHandTools);
-
-
-            toolLibrarySystem.add(tool1, 2);
-
-            //toolLibrarySystem.delete(tool1, 1);
-
-            //toolLibrarySystem.displayTools("Basic Hand Tools");
-            */
-
-            /*
             toolLibrarySystem.add(member1);
             toolLibrarySystem.add(member2);
             toolLibrarySystem.add(member3);
 
             toolLibrarySystem.borrowTool(member1, tool1);
-            toolLibrarySystem.returnTool(member1, tool1);
 
-            toolLibrarySystem.displayBorrowingTools(member1);
-            */
-
-            /*
-            MemberCollection memberCollection = new MemberCollection();
-            ToolCollection toolCollection = new ToolCollection(30);
-            Member member1 = new Member("Navid", "Ahmed", "0450077117", "7777");
-            Tool axe = new Tool("Axe", 4, 4, 0);
-            Tool hammer = new Tool("Hammer", 2, 2, 0);
-            toolCollection.add(axe);
-            toolCollection.add(hammer);
-            member1.addTool(axe);
-            memberCollection.add(member1);
-            
-
-            //Console.WriteLine(memberCollection.Number);
-
-            axe.addBorrower(member1);
-
-            */
-
-
-
-            //MainMenu();
+            new MainMenu();
         }
     }
 }
